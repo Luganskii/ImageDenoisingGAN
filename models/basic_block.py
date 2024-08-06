dbda9febbfcc24f6c083fc3c8677054944ef5a41
Luganskii <luganchick@example.com>
2024-08-06 21:50:39 +0300

    Fix

--- models/basic_block.py
+++ models/basic_block.py
@@ -6,10 +6,11 @@ class _Basic_block(nn.Module):
     Basic module for discriminator
     """
 
-    def __init__(self, channels: int, kernel_size: int, stride: int, padding: int, bias: bool, alpha: float) -> None:
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int, bias: bool, alpha: float) -> None:
+        super().__init__()
         self.redisual_block = nn.Sequential(
-            nn.Conv2d(channels, channels, kernel_size, stride, padding, bias),
-            nn.BatchNorm2d(channels),
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=bias),
+            nn.BatchNorm2d(out_channels),
             nn.LeakyReLU(alpha)
         )
 
